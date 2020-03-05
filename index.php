848490a0f79832df6c0bcf70ef2e2d1147ec24d0
Aubrey Cook <aubreyjcook@gmail.com>
2020-03-05 15:46:33 -0600

    finalized

--- index.php
+++ index.php
@@ -5,28 +5,75 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Assignment 4</title>
+        <title>Assignment 5</title>
         <style>
-            table,
-            td,
-            th {
-                border: 1px solid black;
-                font-weight: bold;
-                text-align: center;
+        .red {
+                color: red;
             }
-            .red {
-                background-color: red;
-            }
-
+        .green {
+            color: green;
+        }
         </style>
     </head>
 
     <body>
-        <h1>Number Chart</h1>
+        <h1>US Census Population Change Calculator</h1>
         <form action="" method="post">
-            <label>Enter Limit:
+            <label for="year1">Year 1:
             </label>
-            <input name="limit" type="number" step="10" min="10" max="500" required>
+            <select id="year1" name="year1">
+                <option value="1790">1790</option>
+                <option value="1800">1800</option>
+                <option value="1810">1810</option>
+                <option value="1820">1820</option>
+                <option value="1830">1830</option>
+                <option value="1840">1840</option>
+                <option value="1850">1850</option>
+                <option value="1860">1860</option>
+                <option value="1870">1870</option>
+                <option value="1880">1880</option>
+                <option value="1890">1890</option>
+                <option value="1900">1900</option>
+                <option value="1910">1910</option>
+                <option value="1920">1920</option>
+                <option value="1930">1930</option>
+                <option value="1940">1940</option>
+                <option value="1950">1950</option>
+                <option value="1960">1960</option>
+                <option value="1970">1970</option>
+                <option value="1980">1980</option>
+                <option value="1990">1990</option>
+                <option value="2000">2000</option>
+                <option value="2010">2010</option>
+            </select>
+            <br>
+            <label for="year2">Year 2:
+            </label>
+            <select id="year2" name="year2">
+                <option value="1790">1790</option>
+                <option value="1800">1800</option>
+                <option value="1810">1810</option>
+                <option value="1820">1820</option>
+                <option value="1830">1830</option>
+                <option value="1840">1840</option>
+                <option value="1850">1850</option>
+                <option value="1860">1860</option>
+                <option value="1870">1870</option>
+                <option value="1880">1880</option>
+                <option value="1890">1890</option>
+                <option value="1900">1900</option>
+                <option value="1910">1910</option>
+                <option value="1920">1920</option>
+                <option value="1930">1930</option>
+                <option value="1940">1940</option>
+                <option value="1950">1950</option>
+                <option value="1960">1960</option>
+                <option value="1970">1970</option>
+                <option value="1980">1980</option>
+                <option value="1990">1990</option>
+                <option value="2000">2000</option>
+                <option value="2010">2010</option>
+            </select>
             <br>
             <br><br>
             <input type="submit" name="submit">
--- output.php
+++ output.php
@@ -1,37 +1,51 @@
 <?php
-$primeArray = array();
-for ($index = 1; $index <= $_POST["limit"]; $index++) {
-    $primeArray[$index] = isPrime($index);
-}
+    $census = array
+    (
+    array("1790",3929214),
+    array("1800",5236631),
+    array("1810",7239881),
+    array("1820",9638453),
+    array("1830",12866020),
+    array("1840",17069453),
+    array("1850",23191876),
+    array("1860",31443321),
+    array("1870",38558371),
+    array("1880",49371340),
+    array("1890",62979766),
+    array("1900",76212168),
+    array("1910",92228531),
+    array("1920",106021568),
+    array("1930",123202660),
+    array("1940",132165129),
+    array("1950",151325798),
+    array("1960",179323175),
+    array("1970",203211926),
+    array("1980",226545805),
+    array("1990",248709873),
+    array("2000",281421906),
+    array("2010",308745538)
+    );
 
-function isPrime($val){ //this function does not extend it's limit beyond the user input $_POST["limit"] since extending the algorithm to find prime numbers beyond this value is greater than the scope of this project
-    if($val == 1)
-    return 0;
-    for ($index = 2; $index <= $_POST["limit"]; $index++) {
-        if($val % $index == 0 && $val !== $index){
-            return 0;
-        }
-    }
-    return 1; 
-}
 
 ?>
 <br>
-<table width="100%">
-    <tr>
-        <th colspan="10">1 - <?php echo $_POST["limit"];?> Prime Numbers are Red</th>
-    </tr>
     <?php
-    $indexGlobal = 1;
-    foreach($primeArray as &$arrayValues){
-        if($indexGlobal + 1 % 10 == 0 || $indexGlobal == 1){echo "<tr>";}
-        if($arrayValues){
-            echo "<td class='red'>$indexGlobal</td>";
-        } else {
-            echo "<td>$indexGlobal</td>";
+        foreach($census as $value){
+            if ("$_POST[year1]" == $value[0])
+                $year1census = $value[1];
+            
+            if ("$_POST[year2]" == $value[0])
+                $year2census = $value[1];
         }
-        if($indexGlobal % 10 == 0){echo "</tr>";}
-        $indexGlobal++;
-    }
+
+        echo "<hr>";
+        echo "Population ", "$_POST[year1]", ": <strong>", number_format($year1census), "</strong> <br>";
+        echo "Population ", "$_POST[year2]", ": <strong>", number_format($year2census), "</strong>";
+        echo "<hr>";
+        if ($year1census > $year2census)
+            echo "<strong class='red'> The Population decreased by ", $year1census - $year2census;
+        else if ($year1census < $year2census)
+            echo "<strong class='green'> The Population increased by ", $year2census - $year1census;
+        else
+            echo "<strong> There was no population change. </strong>";
     ?>
-</table>
